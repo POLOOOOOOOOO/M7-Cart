cb59ea2236688ba9fba1a9bf97162e5c939e1fd8
POLO <polmaciaslopez08@gmail.com>
2024-10-29 18:54:11 +0100

    1.1

--- main.php
+++ main.php
@@ -1,21 +1,24 @@
 <?php
+session_start();
+include_once('com/cart/cart.php');
+include_once('com/user/user.php');
+include_once('com/catalog/catalog.php');
+include_once('com/')
+
+
 if (isset($_GET['action'])) {
     $action = $_GET['action'];
     switch ($action) {
         case 'add_to_cart':
-            include 'cart.php';
             addToCart($_GET['product_id'], $_GET['quantity']);
             break;
         case 'remove_from_cart':
-            include 'cart.php';
             removeFromCart($_GET['product_id']);
             break;
         case 'view_cart':
-            include 'cart.php';
             viewCart();
             break;
         case 'update_cart':
-            include 'cart.php';
             updateCart($_GET['product_id'], $_GET['quantity']);
             break;
     }
